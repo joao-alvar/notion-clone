e1fb3414fa7212507d7acad9b2ecc23f0629cd12
Victor Alvar <victor.alvar@tutanota.com>
2024-06-08 15:47:29 -0300

    Add update auth page, and main.

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -17,7 +17,8 @@ const DashboardPage = async () => {
   if (!user) return
 
   const workspace = await db.query.workspaces.findFirst({
-    where: (workspace, {eq}) => eq(workspace.workspaceOwner, user.id),
+    where: (workspace: {workspaceOwner: any}, {eq}: any) =>
+      eq(workspace.workspaceOwner, user.id),
   })
 
   const {data: subscription, error: subscriptionError} =
